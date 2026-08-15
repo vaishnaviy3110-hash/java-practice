c64800e3d6bdfa51c3ce90cfb49fc3901b0c9550
Vaishnavi Yadav <vaishnaviy3110@gmail.com>
2026-08-15 08:12:44 +0530

    Add Hello World Java program

---
+++ basics/hello.java
@@ -0,0 +1,9 @@
+//Hello world code 
+import java.util.Scanner;
+//we took class name as hello in public
+public class Hello{
+  public static void main(String[] args){
+    System.out.println("Hello,World");  //this part will be printed out 
+  }
+}
+  
