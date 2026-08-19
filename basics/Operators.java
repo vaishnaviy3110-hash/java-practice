7c7cac272d0217070f61716b100a4c15df1c6166
Vaishnavi Yadav <vaishnaviy3110@gmail.com>
2026-08-19 21:30:06 +0530

    Add Operators class demonstrating various operators

---
+++ basics/Operators.java
@@ -0,0 +1,25 @@
+public class Operators {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        // Arithmetic Operators
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        // Relational Operators
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+
+        // Logical Operators
+        System.out.println("AND: " + (a > 5 && b < 10));
+        System.out.println("OR: " + (a > 20 || b < 10));
+        System.out.println("NOT: " + !(a > b));
+    }
+}
