661394e3819602f99894f9a77d0a726b6d9381c7
Vaishnavi Yadav <vaishnaviy3110@gmail.com>
2026-08-23 21:55:57 +0530

    Create DataTypes class with examples of data types
    Added a DataTypes class to demonstrate various Java data types.

---
+++ basics/DataType.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte a = 10;
+        short b = 100;
+        int c = 1000;
+        long d = 100000L;
+
+        float e = 5.5f;
+        double f = 10.25;
+
+        char g = 'V';
+        boolean h = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+} 
